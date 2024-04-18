97fe6e2063493b3b61d79cbcf6b31aa18bca0490
AlphaGogoo <1411420236@qq.com>
2024-04-18 08:13:02 +0800

    等级匹配bug修复

---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+function longestCommonSubstringLength(str1: string, str2: string): number {
+  let result = 0
+  // 创建一个二维数组来存储最长公共子串的长度
+  const dp = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
+
+  // 填充dp数组
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      if ( i == 0 || j == 0){
+        dp[i][j] = 0;
+      }else if(str1[i-1] == str2[j-1]){
+        dp[i][j] = dp[i-1][j-1] + 1;
+        result = Math.max(dp[i][j], result);
+      }else{
+        dp[i][j] = 0;
+      }
+    }
+  }
+  return result
+}
+export {longestCommonSubstringLength}
+
